c75d688e26828077d6c13a34a945892a24de78ef
Macías Romero <macias_george@outlook.com>
2025-03-25 18:34:30 -0600

    [UPDATE] debugging token v10

--- app/api/test-cookies/route.ts
+++ app/api/test-cookies/route.ts
@@ -1,4 +1,33 @@
 export async function GET(req: Request) {
-  console.log(req);
+  const cookie = req.headers.get("cookie");
+  console.log("cookie", cookie);
+
+  console.log(
+    "accces_token",
+    cookie
+      ?.split(";")
+      .find((c) => c.trim().startsWith("access_token="))
+      ?.split("=")
+      .pop()
+  );
+
+  const user = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
+    headers: {
+      // Authorization: `${token?.value}`,
+      Authorization: `${cookie
+        ?.split(";")
+        .find((c) => c.trim().startsWith("access_token="))
+        ?.split("=")
+        .pop()},
+        }`,
+    },
+  });
+
+  if (user.ok) {
+    console.log("user", await user.json());
+  } else {
+    console.log("NO USER");
+  }
+
   return new Response("Hello World", { status: 200 });
 }
